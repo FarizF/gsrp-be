732d738109e8f1b0a3ac3a2ca91e9785d6df55ce
FARIZ-PC\fariz <fariz_fakkel@hotmail.com>
2022-09-18 20:05:25 +0200

    Base BE init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
 import express from "express";
+import mongoose from "mongoose";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 
-console.log("EXPRESS");
+const app = express();
+app.use(express.json());
+
+// adding Helmet to enhance your Rest API's security
+app.use(helmet());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan("combined"));
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World, from express");
+});
+
+app.get("/2xf-discord-auth", (req, res) => {
+    console.log(req, res);
+    res.send("Hello World, from express");
+});
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
