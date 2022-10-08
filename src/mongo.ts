95746d911a7e6377c0981c94d22e309e6528b632
FARIZ-PC\fariz <fariz_fakkel@hotmail.com>
2022-10-09 00:27:36 +0200

    select db differently

--- src/mongo.ts
+++ src/mongo.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export async function connect(): Promise<typeof mongoose> {
     const address = process.env.BLADE + ":" + process.env.MONGO_PORT;
-    const connectionString = `mongodb://${address}/gsrp`;
+    const connectionString = `mongodb://${address}`;
 
-    return await mongoose.connect(connectionString, { user: process.env.MONGO_USER, pass: process.env.MONGO_PW });
+    return await mongoose.connect(connectionString, { user: process.env.MONGO_USER, pass: process.env.MONGO_PW, dbName: "gsrp" });
 }
 
