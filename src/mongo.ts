8b0d316f45b792bad1b0c432f13df59ad36a874a
FARIZ-PC\fariz <fariz_fakkel@hotmail.com>
2022-10-09 00:20:58 +0200

    mongoose use gsrp db

--- src/mongo.ts
+++ src/mongo.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connect(): Promise<typeof mongoose> {
     const address = process.env.BLADE + ":" + process.env.MONGO_PORT;
-    const connectionString = `mongodb://${address}`;
+    const connectionString = `mongodb://${address}/gsrp`;
 
     return await mongoose.connect(connectionString, { user: process.env.MONGO_USER, pass: process.env.MONGO_PW });
 }
