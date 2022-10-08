78b9e18b73db2efffce30f7b99ae4de936599107
FARIZ-PC\fariz <fariz_fakkel@hotmail.com>
2022-10-08 22:15:39 +0200

    Removed middleware

--- src/routes/register.ts
+++ src/routes/register.ts
@@ -3,11 +3,6 @@ import { Router } from "express";
 
 export const router = Router();
 
-router.use((req, res, next) => {
-    console.log("Time: ", Date.now());
-    next();
-});
-
 router.get("/new-player", (req, res) => {
     // let newplayer: Player = {};
     console.log("TEST");
